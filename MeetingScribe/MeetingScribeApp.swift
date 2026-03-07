85f7e1907cdc7dcd3d6f388ce5818e38d033187a
Furuse Shugo <shugoena@gmail.com>
2026-03-07 21:14:53 +0900

    Initial Commit

---
+++ MeetingScribe/MeetingScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingScribeApp.swift
+//  MeetingScribe
+//
+//  Created by Furuse Shugo on 2026/03/07.
+//
+
+import SwiftUI
+
+@main
+struct MeetingScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
